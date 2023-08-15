eb6103dbc2fe969ff57e0e49c5dbf047f7fc7926
srs-sieam <srssieam@gmail.com>
2023-08-15 08:14:11 +0600

    bank page created

--- try.js
+++ try.js
@@ -1,14 +1,17 @@
 document.getElementById('submitBtn').addEventListener('click', function(){
     const emailField = document.getElementById('email');
     const email = emailField.value;
-    if(!email.includes('@')){
-        alert('please provide a valid email')
+    if(!email.includes('@') || email === ''){
+        alert('please provide a valid email');
     }
     const passwordField = document.getElementById('password');
     const password = passwordField.value;
+    if(password === ''){
+        alert('You can not login without password');
+    }
     if(email ==='sontan@gmail.com' && password ==='secret'){
-        console.log('valid user');
+        window.location.href = 'bank.html';
     }else{
-        console.log('invalid user')
+        alert('invalid user')
     }
 })
---
+++ try2.js
@@ -0,0 +1,30 @@
+// total balance
+const totalBalanceElement = document.getElementById('total-balance');
+// deposit
+const depositBalanceElement = document.getElementById('deposit-balance');
+const depositField = document.getElementById('deposit-input');
+
+document.getElementById('deposit').addEventListener('click', function(){ 
+    const newDepositAmount = parseFloat(depositField.value);
+    const previousDepositbalance = parseFloat(depositBalanceElement.innerText);
+    const previousTotalBalance = parseFloat(totalBalanceElement.innerText);
+    const currentDepositBalance = previousDepositbalance + newDepositAmount;
+    const currentTotalBalance = previousTotalBalance + newDepositAmount;
+    depositBalanceElement.innerText = currentDepositBalance;
+    totalBalanceElement.innerText = currentTotalBalance;
+    depositField.value='';
+})
+
+// withdraw
+const withdrawBalanceElement = document.getElementById('withdraw-balance');
+const withdrawField = document.getElementById('withdraw-input');
+document.getElementById('withdraw').addEventListener('click', function(){
+    const newWithdrawAmount = parseFloat(withdrawField.value);
+    const previousWithdrawBalance = parseFloat(withdrawBalanceElement.innerText);
+    const previousTotalBalance = parseFloat(totalBalanceElement.innerText);
+    const currentWithdwawBalance = previousWithdrawBalance + newWithdrawAmount;
+    const currentTotalBalance = previousTotalBalance - newWithdrawAmount;
+    withdrawBalanceElement.innerText = currentWithdwawBalance;
+    totalBalanceElement.innerText = currentTotalBalance;
+    withdrawField.value= '';
+})
